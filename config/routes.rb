ca044afa6d22bafccce0a5c296d862b28cbe431d
shusukeO <sokihara924@gmail.com>
2020-12-14 17:26:59 +0900

    welcomeのcontrollerの削除とmemoindexへのその機能の移行

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'welcome/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'memos#index'
 end
